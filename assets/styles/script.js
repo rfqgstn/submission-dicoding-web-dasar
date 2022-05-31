c8e7ddfcb416c49e853037c22065d2ad8637e41a
rfqgstn <mochamadrifqyagustino@gmail.com>
2022-05-31 08:22:27 +0700

    add some javascript function

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,1 +1,18 @@
-console.log("Halo rifqy")
+const card = document.querySelectorAll(".card");
+
+for (cards of card ){
+	cards.addEventListener('mouseover', function(event){
+		if (event.target.id === "icon") {
+			event.target.style.height = "100px";
+			event.target.style.width = "100px";
+			return;
+		}
+	});
+		cards.addEventListener('mouseout', function (event) {
+        if (event.target.id === "icon") {
+           	event.target.style.height = "80px";
+           	event.target.style.width = "80px";
+            return;
+        }
+	});
+}
